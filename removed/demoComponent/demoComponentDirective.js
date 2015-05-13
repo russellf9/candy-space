e326f352a7779fcdd1136e74236d9bf7a2bbba96
russellf9 <russell@factornine.co.uk>
2015-05-13 12:39:46 +0100

    Add Structure - Trello Done #23
    Create all the required Angular resources
    
    - Remove legacy code

--- removed/demoComponent/demoComponentDirective.js
+++
@@ -1,8 +0,0 @@
-angular.module('wallet-app')
-
-.directive('demoComponent', [function() {
-        return {
-            restrict: 'A',
-            templateUrl: 'components/demoComponent/demoComponent.html'
-        };
-    }]);

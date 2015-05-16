e3ae9ec00db83c68ba1712eb78fe8a2c1f75c2f5
russellf9 <russell@factornine.co.uk>
2015-05-16 17:19:13 +0100

    Wrap up project - Trello Done #26
    Present the app in the best possible way.
    
    - Added comment

--- app/app.js
+++ app/app.js
@@ -1,19 +1,19 @@
 angular.module('posts-app', ['ui.router', 'posts-app.posts', 'posts-app.services']).controller('Posts', Posts)
 
 
-.config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
+    .config(['$stateProvider', '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise('/');
+            $urlRouterProvider.otherwise('/');
 
-        $stateProvider
+            $stateProvider
 
-            .state('home', {
-                url: '/',
-                templateUrl: 'components/home.html',
-                controllerAs: Posts
-            });
-    }]);
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'components/home.html',
+                    controllerAs: Posts
+                });
+        }]);
 
 
 // for now a completely `flat` controller, It could have the responsibility of handling updates
@@ -26,8 +26,9 @@ function Posts($rootScope) {
     function detectUserAgent() {
         var isMobile = navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
 
-        if(!isMobile) {
-           // window.location = "http://www.candyspace.com";
+        if (!isMobile) {
+            // Note: Comment out this line for testing on Desktop
+            window.location = "http://www.candyspace.com";
         }
     }
 

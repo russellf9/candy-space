e98119bbd8cd652462a2d917c62005b5e18b4090
russellf9 <russell@factornine.co.uk>
2015-05-16 10:48:31 +0100

    Add Node BackEnd Trello #23
    - Add the Server.js file
    
    - Add task to run the server
    
    - Add route to get the data

--- app/components/posts/posts.service.js
+++ app/components/posts/posts.service.js
@@ -6,7 +6,7 @@
     'use strict';
 
 
-    angular.module('posts-app.services', []).service('PostsService', ['$q', '$http', function($q, $http) {
+    angular.module('posts-app.services', []).service('PostsService', ['$q', function($q) {
 
         // TODO need a getter for this property to work correctly!
         var _posts;
@@ -16,9 +16,19 @@
             getPosts: function() {
 
                 console.log('\n----\nAccounts::getPosts!');
-                return $http.get('/api/posts').then(function(result) {
-                    return result.data;
-                });
+                // There will always be a promise so always declare it.
+                var deferred = $q.defer();
+
+                // if the initial value is null reset to the default
+                if (!_posts) {
+                    deferred.reject("Error: no data as yet ");
+                }
+
+
+                // Resolve the deferred $q object before returning the promise
+                deferred.resolve({});
+
+                return deferred.promise;
 
             }
 

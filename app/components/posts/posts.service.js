deaf0e7e1f2b64e64ba7b6bd6795c7ca9dcd45a8
russellf9 <russell@factornine.co.uk>
2015-05-16 11:06:47 +0100

    Add Node BackEnd Trello #23
    - Fix the removal of the `api` prefix

--- app/components/posts/posts.service.js
+++ app/components/posts/posts.service.js
@@ -6,7 +6,7 @@
     'use strict';
 
 
-    angular.module('posts-app.services', []).service('PostsService', ['$q', function($q) {
+    angular.module('posts-app.services', []).service('PostsService', ['$q', '$http', function($q, $http) {
 
         // TODO need a getter for this property to work correctly!
         var _posts;
@@ -16,19 +16,9 @@
             getPosts: function() {
 
                 console.log('\n----\nAccounts::getPosts!');
-                // There will always be a promise so always declare it.
-                var deferred = $q.defer();
-
-                // if the initial value is null reset to the default
-                if (!_posts) {
-                    deferred.reject("Error: no data as yet ");
-                }
-
-
-                // Resolve the deferred $q object before returning the promise
-                deferred.resolve({});
-
-                return deferred.promise;
+                return $http.get('/posts').then(function(result) {
+                    return result.data;
+                });
 
             }
 
--- server.js
+++ server.js
@@ -7,17 +7,40 @@ var port = process.env.PORT || 8080; // set our port
 var staticdir = process.env.NODE_ENV === 'production' ? 'dist.prod' : 'dist.dev'; // get static files dir
 
 // get all data/stuff of the body (POST) parameters
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // parse application/json
-//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-//app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+//app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/' + staticdir)); // set the static files location /public/img will be /img for users
 
 // routes ==================================================
 require('./devServer/routes')(app); // configure our routes
 
+
+//var posts
+
+
+// more routes for our API will happen here
+
+var router = express.Router(); // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!'});
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// gets the posts
+app.get('/posts', function(req,res){
+    res.json({data:'todo'});
+});
+
+
+
 // start app ===============================================
-app.listen(port);                   // startup our app at http://localhost:8080
+app.listen(port);                                     // startup our app at http://localhost:8080
 console.log('Starting sever on port ' + port);       // shoutout to the user
-exports = module.exports = app;             // expose app
+exports = module.exports = app;                      // expose app
